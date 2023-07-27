9dabdc46a6bc9ed7dbcd2fb26d20100e5bcdcf70
azeek21 <pymanuz@gmail.com>
2023-07-27 19:59:47 +0300

    fix: netlify env

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL || process.env.BASE_URL;
 
 const ENDPOINT = {
   employees: BASE_URL + "employees",
